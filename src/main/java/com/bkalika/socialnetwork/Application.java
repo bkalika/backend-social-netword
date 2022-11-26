0367bc169d91725fb961dc74afa37d7978422cda
bkalika <bogdan.kalika@gmail.com>
2022-11-26 23:34:49 +0200

    Initial commit

---
+++ src/main/java/com/bkalika/socialnetwork/Application.java
@@ -0,0 +1,14 @@
+package com.bkalika.socialnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author @bkalika
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
